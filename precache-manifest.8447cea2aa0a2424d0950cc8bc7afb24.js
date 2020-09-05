9b4bd7179255b8589072170b1c293cfea0ddfc2f
monstaro <codysmith2814@gmail.com>
2020-09-05 13:18:09 -0600

    Updates

---
+++ precache-manifest.8447cea2aa0a2424d0950cc8bc7afb24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805d31451a2328631d8ffee22d04accb",
+    "url": "/ANTdesign-ToDo/index.html"
+  },
+  {
+    "revision": "4264357d674bac4f60a2",
+    "url": "/ANTdesign-ToDo/static/css/main.51a27126.chunk.css"
+  },
+  {
+    "revision": "88a3c2997c0715c75c92",
+    "url": "/ANTdesign-ToDo/static/js/2.43dcca13.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ANTdesign-ToDo/static/js/2.43dcca13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4264357d674bac4f60a2",
+    "url": "/ANTdesign-ToDo/static/js/main.b9082de0.chunk.js"
+  },
+  {
+    "revision": "1d24972133ad412d2be5",
+    "url": "/ANTdesign-ToDo/static/js/runtime-main.6ffa03d3.js"
+  }
+]);
