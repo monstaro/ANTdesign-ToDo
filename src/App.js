a6409e889e5948904c4547799bcd77c53f528545
monstaro <codysmith2814@gmail.com>
2020-09-04 18:13:49 -0600

    Inserts components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.less';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import MainContainer from './Components/MainContainer';
+import { TasksProvider } from './TasksContext';
+const { Header, Content, Footer } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePage: 'addtask'
+    }
+  }
+  updateActivePage = (currentPage) => {
+    this.setState({
+      activePage: currentPage
+    })
+  }
+  render() {
+    return (
+    <TasksProvider>
+    <Layout className="layout">
+    <Header>
+      <div className="logo" />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+        <Menu.Item key="1" onClick={() => this.updateActivePage('addtask')}>Add Task</Menu.Item>
+        <Menu.Item key="2" onClick={() => this.updateActivePage('alltasks')}>All Tasks</Menu.Item>
+        <Menu.Item key="3" onClick={() => this.updateActivePage('archive')}>Archive</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        {/* <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>List</Breadcrumb.Item>
+        <Breadcrumb.Item>App</Breadcrumb.Item> */}
+      </Breadcrumb>
+      <div className="site-layout-content">
+      <MainContainer activePage={this.state.activePage}/>
+      </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+  </Layout>
+  </TasksProvider>
+    )
+  };
 }
 
 export default App;
